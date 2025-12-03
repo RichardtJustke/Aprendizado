49b03c4687cb13a6ae2e10f9d4253cec1fea5f9d
Richardt Justke <rj.justke@gmail.com>
2025-12-02 21:27:10 -0300

    ataualiza;áo dos estudos de js

---
+++ Linguagens/Aulas JS/Curso YT/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("texto").innerHTML = "Chamada <b> innerHTML </b> realizada com sucesso!";
+
+alert("Olá, Mundo!");
+console.log("Olá, Mundo!");
+
+//peguei a ideia 
+/*document.getElementById("texto").innerHTML = "Chamada <b> innerHTML </b> realizada com sucesso!";*/
