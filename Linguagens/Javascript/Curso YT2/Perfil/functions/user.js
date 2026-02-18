b091d83c401a2a2a19912dcc22ea77b228458072
richardtjustke <rj.justke@gmail.com>
2026-02-18 14:42:52 -0300

    revisar depois exercicio de modulos

---
+++ Linguagens/Javascript/Curso YT2/Perfil/functions/user.js
@@ -0,0 +1,17 @@
+const user = {
+  nome: "Richardt",
+  age: 21,
+  email: "rj.justke@gmial.com",
+  andress: {
+    city: "Brasília",
+    state: "São Sebastião",
+    country: "Rua 06 quadra 11 casa 18",
+  },
+  skill: {
+    sk1: "JS",
+    sk2: "GO",
+    sk3: "Rust",
+  }
+}
+
+export default { user }
---
+++ Linguagens/Javascript/Curso YT2/Perfil/main.js
@@ -0,0 +1,7 @@
+import user from ".functions/user.js"
+
+function main(user) {
+  console.log(user)
+}
+
+
