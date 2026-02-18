dbfaf18a835a32ef9e66be5f5445e4d5f5994533
richardtjustke <rj.justke@gmail.com>
2026-02-18 13:27:50 -0300

    hello word rust

---
+++ Linguagens/Rust/main/target/debug/deps/main-3f510d16865b5004.d
@@ -0,0 +1,5 @@
+/home/richardtjustke/Documentos/code/Aprendizado/Linguagens/Rust/main/target/debug/deps/main-3f510d16865b5004.d: src/main.rs
+
+/home/richardtjustke/Documentos/code/Aprendizado/Linguagens/Rust/main/target/debug/deps/libmain-3f510d16865b5004.rmeta: src/main.rs
+
+src/main.rs:
---
+++ Linguagens/Rust/main/target/debug/deps/main-bf0fb6abc2f238b5.d
@@ -0,0 +1,5 @@
+/home/richardtjustke/Documentos/code/Aprendizado/Linguagens/Rust/main/target/debug/deps/main-bf0fb6abc2f238b5.d: src/main.rs
+
+/home/richardtjustke/Documentos/code/Aprendizado/Linguagens/Rust/main/target/debug/deps/libmain-bf0fb6abc2f238b5.rmeta: src/main.rs
+
+src/main.rs:
