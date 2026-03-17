93b27f479a564846d1ddb481d0fa60ef2c8af4c8
richardtjustke <rj.justke@gmail.com>
2026-03-17 07:05:08 -0300

    meu cli ta foda d+

--- Linguagens/Javascript/AprendendoJs/testes/ex1/bin/index.js
+++ Linguagens/Javascript/AprendendoJs/testes/ex1/bin/index.js
@@ -1,20 +1,33 @@
 #!/usr/bin/env node
 import gradient from "gradient-string"
 
-console.log(gradient(['red', 'orange'])(`
-       █████ █████  █████  █████████  ███████████ █████   ████ ██████████      █████████  █████       █████
-      ▒▒███ ▒▒███  ▒▒███  ███▒▒▒▒▒███▒█▒▒▒███▒▒▒█▒▒███   ███▒ ▒▒███▒▒▒▒▒█     ███▒▒▒▒▒███▒▒███       ▒▒███ 
-       ▒███  ▒███   ▒███ ▒███    ▒▒▒ ▒   ▒███  ▒  ▒███  ███    ▒███  █ ▒     ███     ▒▒▒  ▒███        ▒███ 
-       ▒███  ▒███   ▒███ ▒▒█████████     ▒███     ▒███████     ▒██████      ▒███          ▒███        ▒███ 
-       ▒███  ▒███   ▒███  ▒▒▒▒▒▒▒▒███    ▒███     ▒███▒▒███    ▒███▒▒█      ▒███          ▒███        ▒███ 
- ███   ▒███  ▒███   ▒███  ███    ▒███    ▒███     ▒███ ▒▒███   ▒███ ▒   █   ▒▒███     ███ ▒███      █ ▒███ 
-▒▒████████   ▒▒████████  ▒▒█████████     █████    █████ ▒▒████ ██████████    ▒▒█████████  ███████████ █████
- ▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒    ▒▒▒▒▒   ▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒ 
-                                                                                                           
-                                                                                                           
-                                                                                                           
+console.log(gradient(['red', 'orange']).multiline(`
+
+████████╗███████╗███████╗████████╗███████╗     ██████╗██╗     ██╗
+╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██╔════╝    ██╔════╝██║     ██║
+   ██║   █████╗  ███████╗   ██║   █████╗      ██║     ██║     ██║
+   ██║   ██╔══╝  ╚════██║   ██║   ██╔══╝      ██║     ██║     ██║
+   ██║   ███████╗███████║   ██║   ███████╗    ╚██████╗███████╗██║
+   ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝     ╚═════╝╚══════╝╚═╝
 `)
 )
 
 //npm install chalk figlet gradient-string
-//baixar para deixar bonito ksksksks 
+//https://www.npmjs.com/package/gradient-string(configurações do gradient-string)
+//baixar para deixar bonito ksksksks
+//ir no site ("https://patorjk.com/software/taag/")
+//usar a font ANSI Shadow
+//colocar texto
+const command = process.argv[2]
+if (!command) {
+  console.log(`
+┌─────────────────────────────┐
+│   Bem Vindo ao TESTE CLI    │
+└─────────────────────────────┘
+Aqui é um ambiente de testes onde vou usar para depois fazer meu portifolio abaixo temos
+alguns comandos que você poderá dar no terminal junto com o comando base (ex1)
+Command:
+testepage1
+testepage2
+`)
+}
